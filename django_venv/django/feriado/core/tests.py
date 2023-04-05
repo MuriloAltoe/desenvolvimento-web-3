7cc93ed07128eee240ba0b43f8e4ccd04434d5a6
MuriloAltoe <muriloaleme17@gmail.com>
2023-04-05 19:35:30 -0300

    testes

--- django_venv/django/feriado/core/tests.py
+++ django_venv/django/feriado/core/tests.py
@@ -2,8 +2,16 @@ from django.test import TestCase
 
 class NatalTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get('/')
+        self.resp = self.client.get('/natal')
     def test_200_response(self):
         self.assertEqual(200, self.resp.status_code)
     def test_texto(self):
         self.assertContains(self.resp, 'natal')
+
+class TiradentesTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/tiradentes')
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+    def test_texto(self):
+        self.assertContains(self.resp, 'tiradentes')
