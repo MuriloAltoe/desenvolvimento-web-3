dd00baed6f2e5c91464a8b08c781c2a50c4a1e76
MuriloAltoe <muriloaleme17@gmail.com>
2023-04-05 22:06:13 -0300

    minor-changes

--- django_venv/django/feriado/core/tests.py
+++ django_venv/django/feriado/core/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from core.models import FeriadoModel
+from datetime import datetime
+
 
 class NatalTest(TestCase):
     def setUp(self):
@@ -15,3 +18,15 @@ class TiradentesTest(TestCase):
         self.assertEqual(200, self.resp.status_code)
     def test_texto(self):
         self.assertContains(self.resp, 'tiradentes')
+
+
+def test_created(self):
+    self.assertTrue(FeriadoModel.objects.exists())
+def test_modificado_em(self):
+    self.assertIsInstance(self.cadastro.modificado_em, datetime)
+def test_nome_feriado(self):
+    nome = self.cadastro.__dict__.get('nome', '')
+    self.assertEqual(nome, self.feriado)
+def test_dia_feriado(self):
+    dia = self.cadastro.__dict__.get('dia', '')
+    self.assertEqual(dia, self.dia)
