6b828d8637c562cd8f9275e46de04c3ab078653e
MuriloAltoe <muriloaleme17@gmail.com>
2023-04-16 03:30:55 -0300

    Mudança no banco

--- FATEC_DES_WEB3_mini_avaliacao_3/FATEC_DES_WEB3_mini_avaliacao_3/core/views.py
+++ FATEC_DES_WEB3_mini_avaliacao_3/FATEC_DES_WEB3_mini_avaliacao_3/core/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import render
 from datetime import datetime
 import sqlite3
 
-conn = sqlite3.connect("../db.sqlite3")
+conn = sqlite3.connect("db.sqlite3")
 
 cur = conn.cursor()
 
+# cur.execute("SELECT * FROM core_feriadomodel WHERE dia = " 
+#              + str(datetime.now().day) + " AND mes = " 
+#              + str(datetime.now().month))
+
+cur.execute("SELECT * FROM core_feriadomodel WHERE dia = 25 AND mes = 12")
+
+fetch = cur.fetchall()
+
+contexto = {
+    "feriado": "Não é feriado"
+}
+
+if len(fetch) >= 1:
+    contexto = {
+        "feriado" : str("Hoje é " + fetch[0][1])
+    }
 
 
-
-def natal(request):
-    if datetime.now().month == 12 and datetime.now().day == 25:
-        contexto["natal"] = True
-    return render(request, 'natal.html', contexto)
-
-def tiradentes(request):
-    if datetime.now().month == 4 and datetime.now().day == 21:
-        contexto["tiradentes"] = True
-    return render(request, 'tiradentes.html', contexto)
+def index(request):
+    return render(request, 'index.html', contexto)
--- django_venv/django/feriado/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-path('', views.natal),
-path('natal', views.natal),
-path('tiradentes', views.tiradentes)
-
-
-]
